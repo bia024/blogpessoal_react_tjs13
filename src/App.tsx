5f08b0034b546f660332b1d6612bf425754f3df3
Bianca-Caetano <biancacaetano079@gmail.com>
2026-03-31 11:52:44 -0300

    🎉 Initial Commit: Projeto 2: Criação do Projeto Blog Pessoal - React

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Home from "./pages/home/Home";
+
+function App() {
+  return (
+    <>
+      <Home />
+      {/* <Home /> se eu chamar varias vezes, chama-se empilhamento de Componentes */}
+
+    </>
+  );
+}
+
+export default App;
